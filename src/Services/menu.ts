e53d05ee48c4540c330946081d16f2ffc5d8e2c1
gustavo <gustavopereirafacal@gmail.com>
2023-06-25 19:07:54 -0300

    download image url

--- src/Services/menu.ts
+++ src/Services/menu.ts
@@ -1,7 +1,15 @@
-export const menu = `🌟 *Comandos* 🌟
-    1. ⚡️!help - Exibe a lista de comandos disponíveis.
-    2. ⚡️!everyone - Menciona todos os usuários.
-    3. ⚡️!figurinha - Cria uma figurinha a partir de uma imagem.
-    4. ⚡️ !temperatura e o "nome do lugar" - Mostrarar a temperatura da região. (🎉Funcionalidade nova🎉)
-`
-    
+import { Message } from "whatsapp-web.js";
+
+export async function menu(message: Message) {
+  try {
+    message.reply(
+        `🌟------------------ *Comandos* --------------------🌟
+        1. ⚡️!help - Exibe a lista de comandos disponíveis.
+        2. ⚡️!everyone - Menciona todos os usuários.
+        3. ⚡️!figurinha - Cria uma figurinha a partir de uma imagem.
+        4. ⚡️!temperatura e o "nome do lugar" - Mostrarar a temperatura da região. (🎉Funcionalidade nova🎉)
+    `);
+  } catch (err) {
+    message.reply("❌ Não foi possível executar o menu");
+  }
+}
